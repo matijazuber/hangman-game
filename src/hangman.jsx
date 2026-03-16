db24c2105197b25706a5cc99980a9dae403e48ef
Matija Zuber <matijazuber273@gmail.com>
2026-03-16 15:34:51 +0100

    hardcoded the you win section

--- src/hangman.jsx
+++ src/hangman.jsx
@@ -4,13 +4,20 @@ function Hangman() {
       <header>
         <div>
           <h2>Hangman: Endgame</h2>
-          <p>
+          <p className="hangmanDesc">
             Guess the hidden word in under 8 attempts, each wrong guess brings
             the hangman closer to completion
           </p>
         </div>
       </header>
-      <main></main>
+      <main>
+        <div className="win">
+          <h2 className="winP">You win!</h2>
+          <p className="wellDone">
+            Well done!🎉<img></img>
+          </p>
+        </div>
+      </main>
     </>
   );
 }
