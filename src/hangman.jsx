30619fb63d85d08032831cb5dfd4d15c81383b4e
Matija Zuber <matijazuber273@gmail.com>
2026-03-16 14:52:06 +0100

    created a header section and changed the name of the app.jsx to fit the theme

---
+++ src/hangman.jsx
@@ -0,0 +1,17 @@
+function Hangman() {
+  return (
+    <>
+      <header>
+        <div>
+          <h2>Hangman: Endgame</h2>
+          <p>
+            Guess the hidden word in under 8 attempts, each wrong guess brings
+            the hangman closer to completion
+          </p>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
+}
+export default Hangman;
